ff79b0b0d7351c5f7ed0fe7ea9e5418d8e908ad7
Elisa <elisa.k.perini@gmail.com>
2025-07-10 17:28:08 +0200

    can see container in docker desktop when running test

---
+++ ApiTests/ApiTests.cs
@@ -0,0 +1,76 @@
+using Common.Database;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiTests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        private const string Database = "master";
+        private const string Username = "sa";
+        private const string Password = "$trongPassword";
+        private const ushort MsSqlPort = 1433;
+
+        private WebApplicationFactory<Program> _factory;
+        private IContainer _container;
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            // Set up Testcontainers SQL Server container
+            _container = new ContainerBuilder()
+                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                .WithPortBinding(MsSqlPort, true)
+                .WithEnvironment("ACCEPT_EULA", "Y")
+                .WithEnvironment("SQLCMDUSER", Username)
+                .WithEnvironment("SQLCMDPASSWORD", Password)
+                .WithEnvironment("MSSQL_SA_PASSWORD", Password)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(MsSqlPort))
+                .Build();
+
+            //Start Container
+            await _container.StartAsync();
+
+            var host = _container.Hostname;
+            var port = _container.GetMappedPublicPort(MsSqlPort);
+
+            // Replace connection string in DbContext
+            var connectionString = $"Server={host},{port};Database={Database};User Id={Username};Password={Password};TrustServerCertificate=True";
+            _factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.AddDbContext<FinancesDbContext>(options =>
+                            options.UseSqlServer(connectionString));
+                    });
+                });
+
+            // Initialize database
+            using var scope = _factory.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<FinancesDbContext>();
+            dbContext.Database.Migrate();
+        }
+
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            await _container.StopAsync();
+            await _container.DisposeAsync();
+            _factory.Dispose();
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("api/generic/ping");
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(content, Is.Not.Null);
+        }
+    }
+}
