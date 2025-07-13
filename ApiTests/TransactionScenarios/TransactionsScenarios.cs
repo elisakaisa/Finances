dec58edb2193da0553eda5e00d514be029210a12
Elisa <elisa.k.perini@gmail.com>
2025-07-13 11:35:31 +0200

    added mopre transaction scenarios for monthly

--- ApiTests/TransactionScenarios/TransactionsScenarios.cs
+++ ApiTests/TransactionScenarios/TransactionsScenarios.cs
@@ -129,5 +129,91 @@ namespace ApiTests.TransactionScenarios
             Assert.That(output, Is.Not.Null);
             Assert.That(output.Count, Is.EqualTo(2));
         }
+
+        [Test]
+        public async Task GetMonthlyTransactions_UserInHouseholdWithTwoTransactionsInOtherMonth_ReturnsEmptyList()
+        {
+            // Arrange
+            var financialMonth = "202507";
+            var household = Fixture.Build<Household>().Create();
+            var user = Fixture.Build<User>().With(x => x.HouseholdId, household.Id).With(x => x.Household, household).Create();
+            var category = Fixture.Build<Category>().Create();
+            var subcategory = Fixture.Build<Subcategory>().With(x => x.CategoryId, category.Id).Create();
+            var transaction1 = Fixture.Build<Transaction>()
+                .With(x => x.UserId, user.Id)
+                .With(x => x.User, user)
+                .With(x => x.SubcategoryId, subcategory.Id)
+                .Create();
+            var transaction2 = Fixture.Build<Transaction>()
+                .With(x => x.UserId, user.Id)
+                .With(x => x.User, user)
+                .With(x => x.SubcategoryId, subcategory.Id)
+                .Create();
+
+            await ExecuteScopedContextAction(context =>
+            {
+                context.Categories.Add(category);
+                context.Subcategories.Add(subcategory);
+                context.Users.Add(user);
+                context.Households.Add(household);
+                context.Transactions.Add(transaction1);
+                context.Transactions.Add(transaction2);
+            });
+
+            var requestUri = $"{_transactionBaseUrl}/household/monthly-transactions?financialMonth={financialMonth}";
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            requestMessage.Headers.Add("requestingUserId", user.Id.ToString());
+
+            // Act
+            var response = await HttpClient.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content, Is.EqualTo("[]"));
+        }
+
+        [Test]
+        public async Task GetMonthlyTransactions_UserInHouseholdWithTransactionsInOtherHousehold_ReturnsEmptyList()
+        {
+            // Arrange
+            var financialMonth = "202507";
+            var household = Fixture.Build<Household>().Create();
+            var user = Fixture.Build<User>().With(x => x.HouseholdId, household.Id).With(x => x.Household, household).Create();
+            var category = Fixture.Build<Category>().Create();
+            var subcategory = Fixture.Build<Subcategory>().With(x => x.CategoryId, category.Id).Create();
+            var transaction1 = Fixture.Build<Transaction>()
+                .With(x => x.SubcategoryId, subcategory.Id)
+                .With(x => x.FinancialMonth, financialMonth)
+                .Create();
+            var transaction2 = Fixture.Build<Transaction>()
+                .With(x => x.SubcategoryId, subcategory.Id)
+                .With(x => x.FinancialMonth, financialMonth)
+                .Create();
+
+            await ExecuteScopedContextAction(context =>
+            {
+                context.Categories.Add(category);
+                context.Subcategories.Add(subcategory);
+                context.Users.Add(user);
+                context.Households.Add(household);
+                context.Transactions.Add(transaction1);
+                context.Transactions.Add(transaction2);
+            });
+
+            var requestUri = $"{_transactionBaseUrl}/household/monthly-transactions?financialMonth={financialMonth}";
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            requestMessage.Headers.Add("requestingUserId", user.Id.ToString());
+
+            // Act
+            var response = await HttpClient.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content, Is.EqualTo("[]"));
+        }
     }
 }
